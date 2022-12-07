4b8119322a28fdde8f03621366bbbed25183c9a5
WCalda <WilliamNCalda@gmail.com>
2022-12-07 23:57:19 +0800

    Adding Animation and CSS, DOM Manipulation

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-document.getElementById('downloadbtn').addEventListener('click', (param1) =>{
-    param1.preventDefault();
-    document.querySelector('.hidden').classList.toggle('visible');
+document.getElementById('downloadbtn').addEventListener('click', (a) =>{
+    a.preventDefault();
+
+    document.querySelector('.hidden').classList.replace('hidden', 'visible');
+
+    var el = document.createElement('a');
+    el.setAttribute('href', 'assets/');
+    el.setAttribute('download', 'Resume-WilliamCalda.pdf');
+    el.click();
+
 })
