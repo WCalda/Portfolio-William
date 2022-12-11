6e68b07d7afd18c21cba9a5dff9318f5361f91a7
WCalda <WilliamNCalda@gmail.com>
2022-12-11 14:15:18 +0800

    Additional JS Changes and Additional Pic in Assets Folder

--- main.js
+++ main.js
@@ -9,3 +9,41 @@ document.getElementById('downloadbtn').addEventListener('click', (a) =>{
     el.click();
 
 })
+
+let clickListener = 0;
+
+document.getElementById("nxt").addEventListener('click', ()=>{
+    const srcpath = 'assets/';
+    
+    const Pictures = [
+        'pic-1.jpg',
+        'pic-2.jpg',
+        'pic-3.jpg',
+        'pic-4.jpg',
+        'pic-5.jpg',
+        'pic-6.jpg',
+        'pic-7.jpg'
+    ]
+
+    const Text = [
+        'I have a hobby in Mural Painting and have been painting ever since Senior High School',
+        'i have entered multiple Mural Painting Competitions over the years and have been improving my skills in it',
+        'I also have painted Schools for the Lettering and Logos as well as multiple barangay outpost in our Barangay',
+    ]
+
+    clickListener = clickListener + 1 < Pictures.length ? clickListener + 1 : 0;
+
+    document.getElementById('side-pic').setAttribute(
+        'src', `${srcpath}${Pictures[clickListener]}`
+    )
+
+    let newElement = document.createElement('p');
+
+    newElement.className = 'ntext';
+
+    newElement.append(document.createTextNode(`${Text[clickListener]}`));
+
+    document.getElementById('tobeExtended').appendChild(newElement);
+
+    console.log(document.getElementById('tobeExtended'));
+})
